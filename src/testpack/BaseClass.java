e26fa7b1b083f58125ba28b0363c45bc7bc9c273
teamlead11 <automationlead11@gmail.com>
2018-09-13 12:08:06 +0530

    made some chnages

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -5,6 +5,7 @@ public class BaseClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is my base class");
+		System.out.println("some chnages");
 	}
 
 }
